72d70f3cdd0d720b49eceb4be474a0c01d53128a
a24jauhurgon <a24jauhurgon@inspedralbes.cat>
2025-09-23 08:35:27 +0200

    header cors

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 const NPREGUNTAS=3
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 let estatDeLaPartida = {
     contadorPreguntes: 0,
     respostesUsuari: []
